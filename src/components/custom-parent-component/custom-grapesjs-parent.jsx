baec860c9446cf9bcd447f5219137b439d00fb2f
Parth Chawande <parthrc00@gmail.com>
2024-07-16 10:34:14 +0530

    can added new div at user's click position in the list

--- src/components/custom-parent-component/custom-grapesjs-parent.jsx
+++ src/components/custom-parent-component/custom-grapesjs-parent.jsx
@@ -1,47 +1,74 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./custom-grapesjs-parent.css";
 
 const demottexts = [];
 
 const CustomGrapesjsParent = () => {
-  const [text, setText] = useState("");
-  const [editing, setEditing] = useState(false);
+  const [divs, setDivs] = useState(["Div 1", "Div 2", "Div 3"]);
+  // Ref for parent div
+  const parentRef = useRef();
+  // ref for number of children divs
+  const divRefs = useRef([]);
 
-  const handleClick = () => {
-    setEditing(true);
-  };
+  useEffect(() => {
+    // Clear the divRefs array and reassign refs to current divs
+    divRefs.current = divRefs.current.slice(0, divs.length);
+  }, [divs]);
 
-  const handleBlur = () => {
-    setEditing(false);
-  };
+  // main function to check if user clicks on blank space
+  const handleParentClick = (e) => {
+    // Check if the click target is the parent div itself
+    if (e.target !== parentRef.current) {
+      return;
+    }
+    // get size and position of parent div
+    const parentRect = parentRef.current.getBoundingClientRect();
+    // get y acis value of user click
+    const clickY = e.clientY - parentRect.top;
+    // defualt position to add new child
+    let insertIndex = divs.length;
 
-  const handleChange = (e) => {
-    setText(e.target.value);
+    // loop through all the children divs
+    // to Find the index where the new div should be inserted
+    for (let i = 0; i < divRefs.current.length; i++) {
+      // get values of current div
+      const div = divRefs.current[i];
+      // get size and position of the div
+      const divRect = div.getBoundingClientRect();
+      // check if user click was above this div
+      if (clickY < divRect.top) {
+        insertIndex = i;
+        break;
+      }
+    }
+
+    // Insert the new div at the calculated position
+    setDivs((prevDivs) => [
+      ...prevDivs.slice(0, insertIndex),
+      `New Div ${Date.now()}`,
+      ...prevDivs.slice(insertIndex),
+    ]);
   };
 
   return (
-    <div className="customGrapesjs">
-      Custom grapesjs parent component
-      <div
-        onClick={handleClick}
-        style={{
-          padding: "20px",
-          border: "1px solid black",
-          minHeight: "100px",
-        }}
-      >
-        {editing ? (
-          <input
-            type="text"
-            value={text}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            autoFocus
-          />
-        ) : (
-          text || "Click to edit"
-        )}
-      </div>
+    <div
+      ref={parentRef}
+      onClick={handleParentClick}
+      style={{ border: "1px solid black", padding: "10px" }}
+    >
+      {divs.map((div, index) => (
+        <div
+          key={index}
+          ref={(el) => (divRefs.current[index] = el)}
+          style={{
+            margin: "5px",
+            padding: "10px",
+            backgroundColor: "lightgray",
+          }}
+        >
+          {div}
+        </div>
+      ))}
     </div>
   );
 };
